8fd4185b0803480c12e67a18bb61c311e111e221
Diego Vilhalva <diegorocha450@yahoo.com>
2025-09-04 12:46:22 -0300

    create shop route

---
+++ backend/routes/shop.route.js
@@ -0,0 +1,9 @@
+import express from "express"
+import {authMiddleware} from "../middlewares/auth.js"
+import { upload } from "../middlewares/multer.js"
+import { createShop, getMyShop } from "../controllers/shop.controller.js"
+const router = express.Router()
+
+router.post("/create-edit",authMiddleware,upload.single('image'),createShop)
+router.get("/my-shop",authMiddleware,getMyShop)
+export default router
